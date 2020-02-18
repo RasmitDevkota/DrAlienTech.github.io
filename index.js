ca98db30dfb537fc0c3b514d62e44ea5c8c42aac
Dr AlienTech <rdatch101@gmail.com>
2020-02-17 21:04:33 -0500

    Gitacora saved 3 changed files

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-app.get('/', (req, res) => res.send('Hello World!'))
+server.listen(80);
+// WARNING: app.listen(80) will NOT work here!
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
